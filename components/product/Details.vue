682b043e76d631cc25cf0a3534f41f36028d664e
Romel Indemne <romel.i@gagroup.net>
2022-11-17 09:01:41 +0400

    upgraded to nuxt 3.0.0

--- components/product/Details.vue
+++ components/product/Details.vue
@@ -1,60 +1,63 @@
 <template>
-    <div>
-        <div class="product-wrapper">
-            <div class="product-image">
-                <img :src="`${product.image}`" :alt="product.title">
-            </div>
-            <div class="product-details">
-                <div class="title">{{ product.title }}</div>
-                <div class="price">Price - ${{ product.price }}</div>
-                <div>Description</div>
-                <hr>
-                <div class="description">{{ product.description }}</div>
-            </div>
+  <div>
+    <div class="product-wrapper">
+      <div class="product-image">
+        <img :src="`${product.image}`" :alt="product.title" />
+      </div>
+      <div class="product-details">
+        <div class="title">{{ product.title }}</div>
+        <div class="price">Price - ${{ product.price }}</div>
+        <div>Description</div>
+        <hr />
+        <div class="description">{{ product.description }}</div>
+        <div class="add-to-cart">
+          <v-btn color="secondary" class="text-white">Add to cart</v-btn>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-const { product } = defineProps(['product'])
+const { product } = defineProps(["product"]);
 </script>
 
 <style scoped>
 .product-wrapper {
-    display: flex;
-    background: white;
-    box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.1);
-    padding: 12px;
-    border-radius: 8px;
+  display: flex;
+  background: white;
+  box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.1);
+  padding: 12px;
+  border-radius: 8px;
 }
 
 .product-wrapper .product-image {
-    width: 40%;
-    padding: 12px;
+  width: 40%;
+  padding: 12px;
 }
 
 .product-wrapper .product-image img {
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 }
 
 .product-wrapper .product-details {
-    width: 60%;
-    padding: 12px;
+  width: 60%;
+  padding: 12px;
 }
 
 .product-wrapper .product-details .title {
-    font-size: 3em;
-    margin-bottom: 15px;
+  font-size: 3em;
+  margin-bottom: 15px;
 }
 
 .product-wrapper .product-details .price {
-    font-size: 2em;
-    margin-bottom: 15px;
+  font-size: 2em;
+  margin-bottom: 15px;
 }
 
 .product-wrapper .product-details .description {
-    font-size: 1em;
-    margin-bottom: 15px;
+  font-size: 1em;
+  margin-bottom: 15px;
 }
 </style>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,13 +1,25 @@
 // plugins/vuetify.js
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-export default defineNuxtPlugin(nuxtApp => {
+export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
-  })
+    theme: {
+      defaultTheme: "melTheme",
+      themes: {
+        melTheme: {
+          dark: false,
+          colors: {
+            primary: "#000000",
+            secondary: "#c7a92e",
+          },
+        },
+      },
+    },
+  });
 
-  nuxtApp.vueApp.use(vuetify)
-})
+  nuxtApp.vueApp.use(vuetify);
+});
