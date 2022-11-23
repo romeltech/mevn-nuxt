390eb610425ff1dbb527bd3e8a15779bab3b804f
Romel Indemne <romel.i@gagroup.net>
2022-11-23 10:04:57 +0400

    updated errorpage

--- error.vue
+++ error.vue
@@ -1,15 +1,32 @@
 <template>
-    <div>
-        <div>
-            {{ error.statusCode }}
-        </div>
-        <div>
-            {{ error.message }}
-        </div>
-    </div>
+    <v-app id="inspire" class="error-container">
+        <v-main>
+            <v-container class="fluid">
+                <v-row style="min-height: 100vh;">
+                    <div class="ma-auto text-center">
+                        <div class="text-h1 font-weight-bold">
+                            {{ error.statusCode }}
+                        </div>
+                        <div>
+                            {{ error.message }}
+                        </div>
+                        <v-btn :to="'/'" class="mt-5" width="100%" color="primary" size="large">HOME</v-btn>
+                    </div>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script setup>
 defineProps(['error'])
 
 </script>
+
+<style scoped>
+.error-container {
+    width: 100%;
+    background: rgb(0, 0, 0);
+    background-image: linear-gradient(to right top, #9e8626, #ac9229, #bb9f2c, #caab30, #d9b833, #dcbb34, #e0be34, #e3c135, #dcbb33, #d5b531, #ceaf30, #c7a92e);
+}
+</style>
