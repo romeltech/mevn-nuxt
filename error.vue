79b77ba23b83058296379d1a81375f8ebbac1bf5
Romel Indemne <romel.i@gagroup.net>
2022-11-23 08:39:29 +0400

    Added error page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>
+            {{ error.statusCode }}
+        </div>
+        <div>
+            {{ error.message }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps(['error'])
+
+</script>
